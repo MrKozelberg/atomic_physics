0f4f5ad42cef7c97dc7cbb730b345740b24337ac
MrKozelberg <Mr.Kozelberg@yandex.ru>
2021-11-14 14:58:02 +0300

    -mistakes in franck-hertz-exp#1

--- franck_hertz_experiment/scripts/1.py
+++ franck_hertz_experiment/scripts/1.py
@@ -7,25 +7,26 @@ Created on Sat Oct 30 19:32:19 2021
 """
 
 import matplotlib.pyplot as plt
-
 import numpy as np
 
-from scipy.constants import e
-
-def plotting(data):
+def plotting():
+    data = np.genfromtxt("../data/1.txt")
     fig = plt.figure(figsize=(7,5))
     ax = fig.add_subplot(111)
-    ax.errorbar(data[:,0], data[:,1]*50, xerr=1/2, yerr=0.5/2, c="gray",
+    ax.errorbar(data[:,0], data[:,1]*50, xerr=0.5, yerr=0.25, c="gray",
                 lw=1, ecolor="red")
     ax.set_ylabel("Анодный ток, мА")
-    ax.set_xlabel("Ускоряющее напряжение, В")
+    ax.set_xlabel("Ускоряющий потенциал, В")
+    ax.set_xlim([10,62])
+    ax.set_ylim(2,20)
+    ax.set_xticks([12,20,24,30,40,48,50,60])
+    ax.set_xticklabels(['12','20',r'$\varphi_1$','30','40',r'$\varphi_2$','50','60'])
+    ax.vlines(24, 2, 0.37*50, linestyle=':', color='black')
+    ax.vlines(48, 2, 0.38*50, linestyle=':', color='black')
     # ax.set_title("Анодно-сеточная характеристика\nпри задерживающем напряжении 12.1 В")
     fig.tight_layout()
     fig.savefig("../plots/1.pdf")
 
 if __name__ == "__main__":
-    data = np.genfromtxt("../data/1.txt")
-    plotting(data)
-    # We could find first two local maxima on 24 and 48 V
-    print('E_1 - E_0 = {:.3} эВ'.format(24*e))
+    plotting()
     
